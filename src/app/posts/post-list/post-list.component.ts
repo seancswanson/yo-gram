e82a5b09be387979323aaf28796818cdb73c0e6a
Sean Swanson <sean-swan@hotmail.com>
2020-12-11 08:54:42 -0800

    Post creation is implemented.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-post-list",
   templateUrl: "./post-list.component.html",
   styleUrls: ["./post-list.component.scss"],
 })
-export class PostListComponent {}
+export class PostListComponent {
+  @Input() posts = [];
+}
