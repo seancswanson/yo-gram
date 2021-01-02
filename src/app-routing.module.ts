9773ea468b88c8d2f494ee8c8a8e5642397a7e4d
Sean Swanson <sean-swan@hotmail.com>
2021-01-01 16:02:57 -0800

    Added routing module to define routes. Now rendering each component on its own route. Added nav links to the header bar.

---
+++ src/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PostCreateComponent } from "./app/posts/post-create/post-create.component";
+import { PostListComponent } from "./app/posts/post-list/post-list.component";
+
+// Array of routes with its path and the component to be rendered
+const routes: Routes = [
+  { path: "", component: PostListComponent },
+  { path: "create", component: PostCreateComponent },
+];
+
+@NgModule({
+  imports: [RouterModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
